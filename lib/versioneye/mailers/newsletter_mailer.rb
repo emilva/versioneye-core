f4de5211e0f30aa4d855a01d612d3451e732909f
reiz <robert.reiz.81@gmail.com>
2017-10-26 19:10:40 +0200

    Add new newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => "Shutting down operations by End of 2017") do |format|
+    mail(:to => @user.email, :subject => "VersionEye Sunset & LivePerson") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
